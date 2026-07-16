649e014062dff9126a859efb7447682d4dc7ba3a
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-07-16 15:02:48 -0300

    adiciona página de evento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { Contato } from './feats/contato/contato';
 import { Home } from './feats/home/home';
 import { MenuResponsivo } from './feats/menu-responsivo/menu-responsivo';
+import { Event } from './feats/event/event';
 
 export const routes: Routes = [
 
     { path: 'contato', component: Contato },
     { path: 'home', component: Home },
     { path: 'menu-responsivo', component: MenuResponsivo },
+    { path: 'event', component: Event },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 
 ];
