4ef37487b5ef775bf2819f5cee39595960fc1589
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-07-21 16:03:01 -0300

    Adiciona exemplo de signals

--- src/app/feats/exercicios/exercicios.ts
+++ src/app/feats/exercicios/exercicios.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Contador } from "./contador/contador";
 import { Autenticacao } from "./autenticacao/autenticacao";
 import { ImagemDinamica } from "./imagem-dinamica/imagem-dinamica";
+import { ExemploSignal } from "./exemplo-signal/exemplo-signal";
 
 @Component({
   selector: 'app-exercicios',
-  imports: [Contador, Autenticacao, ImagemDinamica],
+  imports: [Contador, Autenticacao, ImagemDinamica, ExemploSignal],
   templateUrl: './exercicios.html',
   styleUrl: './exercicios.css',
 })
